8a142e44e599d67353c84ea15fb1a7c98646992a
da-solovev <solo.dmit1994@gmail.com>
2024-07-15 22:12:26 +0300

    Add Eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
-import { askUserName } from "../src/cli.js";
+import askUserName from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 askUserName();
