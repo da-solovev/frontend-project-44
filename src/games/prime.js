36d9e7e8d2d70292815a47fcb6adf8423655082e
da-solovev <solo.dmit1994@gmail.com>
2024-07-21 23:06:11 +0300

    Fix Brain-prime "number 1 not prime"

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,6 +8,10 @@ import {
 const rules = ['Answer "yes" if given number is prime. Otherwise answer "no".', 'yes/no'];
 
 const isPrimeNumber = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
